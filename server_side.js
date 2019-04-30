8e9ebbd6a0ce04ba0fb470b4b47feb0391beb75e
Simon Light <slight@cisco.com>
2019-04-30 12:21:37 +0000

    Added multiple lines to graph

--- server_side.js
+++ server_side.js
@@ -9,54 +9,84 @@ var plotly = require('plotly')("slight123", "2NcGLDisE7ryQldFBNCP");
 
 
 const server = app.listen(7000, () => {
-	console.log(`Express running → PORT ${server.address().port}`);
+    console.log(`Express running → PORT ${server.address().port}`);
 });
 
 MongoClient.connect(url, {useNewUrlParser: true}, function(err, client){
-	console.log("Connected");
-	var db = client.db('bees');
-	var collection = db.collection('BeeData');
-	var data = collection.find().toArray(function(err, result) {
-		app.get('/data/', (req, res) => {
-			res.render('index.ejs', {
-				title: 'Data',
-				message: 'Shown data as table, navigate to / for visual display',
-				hive_data: result
-			});
-		});
-		console.log(result);
-		console.log("THIS");
+    console.log("Connected");
 
-		var dates = [];
-		var inTemp = [];
-		var outTemp = [];
-		var humidity = [];
-		console.log("Made it to loop");
-		for (var i = 0; i < result.length; i++){
-			dates.push(result[i].dateTime);
-			humidity.push(result[i].Humidity);
-		};
-		console.log(dates);
-		console.log(humidity);
-		var graph_data = [
-			{
-				x: dates,
-				y: humidity,
-				type: "scatter"
-			}
-		];
-		var graphOptions = {filename: "date-axes", fileopt: "overwrite"};
-		plotly.plot(graph_data, graphOptions, function (err, msg) {
-			console.log(msg);
-		});
+    UpdateTable(client);
+    console.log(client);
+    console.log("THIS IS ALSO IT");
 
+    app.get('/', (req, res) => {
 
-		app.get('/', (req, res) => {
-			res.render('graph.ejs', {
+        console.log(client);
+        console.log("THIS IS IT");
+        UpdateTable(client);
+        res.render('graph.ejs', {
 
-			});
-		});
+        });
+    });
 
-	});
-	client.close();
 });
+
+
+function UpdateTable(client){
+    var db = client.db('bees');
+    var collection = db.collection('BeeData');
+    var data = collection.find().toArray(function(err, result) {
+
+        console.log(result);
+        console.log("THIS |");
+
+        var dates = [];
+        var inTemp = [];
+        var outTemp = [];
+        var humidity = [];
+        for (var i = 0; i < result.length; i++){
+            dates.push(result[i].dateTime);
+            humidity.push(result[i].Humidity);
+            inTemp.push(result[i].inTemp);
+            outTemp.push(result[i].outTemp);
+        };
+        console.log(dates);
+        console.log(humidity);
+        console.log(inTemp);
+        console.log(outTemp);
+        var humidity_trace = 
+            {
+                x: dates,
+                y: humidity,
+                name: "Humidity"
+            };
+        var inTemp_trace = 
+            {
+                x: dates,
+                y: inTemp,
+                name: "Inside Temp"
+            };
+        var outTemp_trace = 
+            {
+                x: dates,
+                y: outTemp,
+                name: "Outside Temp"
+            };
+
+        graph_data = [humidity_trace, inTemp_trace, outTemp_trace];
+
+        var graphOptions = {filename: "date-axes", fileopt: "overwrite"};
+        plotly.plot(graph_data, graphOptions, function (err, msg) {
+            console.log(msg);
+        });
+
+        app.get('/data/', (req, res) => {
+            res.render('index.ejs', {
+                title: 'Data',
+                message: 'Shown data as table, navigate to / for visual display',
+                hive_data: result
+            });
+        });
+
+    });
+}
